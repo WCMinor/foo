994bc94213428131ce1d8f643579b6894bc7ad07
WCMinor <risingofthemoon@gmail.com>
2011-04-20 12:01:36 +0200

    reviewed with reviewer

--- prompt.py
+++ prompt.py
@@ -4,7 +4,7 @@
 ## This script has been designed to be called instead of bash when starting a terminal
 ## it's only useful for sysadmins who use to work remotely most of time
 
-## Defining all the functions
+## Defining the bash function
 
 import os
 
@@ -12,75 +12,27 @@ def bash():
 	os.system('/bin/bash')
 	quit()
 
-def briana():
-	os.system('ssh root@briana.traci.es')
-	quit()
-
-def belladona():
-	os.system('ssh root@belladona.traci.es')
-	quit()
-
-def ecodes():
-	os.system('ssh root@ecodes.org')
-	quit()
-
-def ns3():
-	os.system('ssh root@ns3.traci.es')
-	quit()
-
-def panel():
-	os.system('ssh root@panel.traci.es')
-	quit()
-
-def panel2():
-	os.system('ssh root@panel2.traci.es')
-	quit()
-
-def hispalinux():
-	os.system('ssh -p 22022 root@pound.hispalinux.es')
-	quit()
-
-def correo():
-	os.system('ssh root@correo.traci.es')
-	quit()
-
-def srv1():
-	os.system('ssh -i ~/.ssh/id_rsa.bifi root@srv1.ibercivis.es')
-	quit()
-
-def srv2():
-	os.system('ssh -i ~/.ssh/id_rsa.bifi root@srv2.ibercivis.es')
-	quit()
-
-def srv3():
-	os.system('ssh -i ~/.ssh/id_rsa.bifi root@srv3.ibercivis.es')
-	quit()
-
-def srv4():
-	os.system('ssh -i ~/.ssh/id_rsa.bifi root@srv4.ibercivis.es')
-	quit()
-
 ## We create a python's dictionary with all the options, note that each name is one function
 
-options ={	0 : briana,
-		1 : belladona,
-		2 : ecodes,
-		3 : panel, 
-		4 : panel2, 
-		5 : ns3, 
-		6 : hispalinux, 
-		7 : correo, 
-		8 : srv1, 
-		9 : srv2, 
-		10 : srv3, 
-		11 : srv4, 
+options ={	0 : ['briana', 'ssh root@briana.traci.es'],
+		1 : ['belladona', 'ssh root@briana.traci.es'],
+		2 : ['ecodes', 'ssh root@ecodes.org'], 
+		3 : ['panel',  'ssh root@panel.traci.es'],
+		4 : ['panel2', 'ssh root@panel2.traci.es'], 
+		5 : ['ns3',  'ssh root@ns3.traci.es'], 
+		6 : ['hispalinux', 'ssh -p 22022 root@pound.hispalinux.es'], 
+		7 : ['correo',  'ssh root@correo.traci.es'], 
+		8 : ['srv1', 'ssh -i ~/.ssh/id_rsa.bifi root@srv1.ibercivis.es'],
+		9 : ['srv2', 'ssh -i ~/.ssh/id_rsa.bifi root@srv2.ibercivis.es'], 
+		10 : ['srv3', 'ssh -i ~/.ssh/id_rsa.bifi root@srv3.ibercivis.es'], 
+		11 : ['srv4', 'ssh -i ~/.ssh/id_rsa.bifi root@srv4.ibercivis.es'] 
 }
 
 ## Print all the options in the dic 
 
 print "These are the traveling options:\n"
-for k, d in options.iteritems():
-	print k, options[k].func_name
+for k in options.keys():
+	print k, options[k][0]
 print "\nOr any key + enter to execute bash\n"
 
 ## Get a parameter prevent any error if the human doesn't type a number
@@ -95,16 +47,15 @@ ande = ira()
 
 ##call the correspondent function
 
-for i in range(12):
-	if i == ande:
-		options[i]()
+if ande in options.keys():
+	os.system(options[ande][1])
+else:
+	# If noth0ing else was selected, execute bash
 
-# If noth0ing else was selected, execute bash
+	print 'executing bash intepreter'
+	import time
+	time.sleep(1)
 
-print 'executing bash intepreter'
-import time
-time.sleep(1)
-
-bash()
+	bash()
 
 quit()
