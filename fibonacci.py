a70c5be35bac759b8616206b40d10755a270a087
WCMinor <risingofthemoon@gmail.com>
2011-05-03 12:00:50 +0200

    fibonacci example now works

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
-def fib(n):    # write Fibonacci series up to n
+
+# write Fibonacci series up to n
+def fib(n):
 	a, b = 0, 1
 	while b < n:
 		a, b = b, a+b
-		return b
-print fib(int(raw_input("Print a Fibonacci series up to whatever you enter here:\n")))
+		print a
+fib(int(raw_input("Print a Fibonacci series up to whatever you enter here:\n")))
