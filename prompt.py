6316a47d5a805ea71afbc7de18a319ae1d2a36aa
WCMinor <risingofthemoon@gmail.com>
2012-03-07 15:00:38 +0000

    bla bla

--- prompt.py
+++ prompt.py
@@ -9,7 +9,7 @@
 import os
 
 def bash():
-	os.system('bash')
+	os.system('/opt/local/bin/bash')
 
 ## We create a python's dictionary with all the options, note that each name is one function
 
@@ -18,15 +18,22 @@ options ={	'0' : ['briana', 'ssh -i ~/.ssh/id_rsa.finito root@briana.traci.es'],
 		'2' : ['ecodes', 'ssh -i ~/.ssh/id_rsa.finito root@ecodes.org'], 
 		'3' : ['panel',  'ssh -i ~/.ssh/id_rsa.finito root@panel.traci.es'],
 		'4' : ['panel2', 'ssh -i ~/.ssh/id_rsa.finito root@panel2.traci.es'], 
-		'5' : ['ns3',  'ssh -i ~/.ssh/id_rsa.finito root@ns3.traci.es'], 
-		'6' : ['hispalinux', 'ssh -p 22022 -i ~/.ssh/id_rsa.finito root@pound.hispalinux.es'], 
-		'7' : ['correo',  'ssh -i ~/.ssh/id_rsa.finit oroot@correo.traci.es'], 
-		'8' : ['srv1', 'ssh -i ~/.ssh/id_rsa.bifi root@srv1.ibercivis.es'],
-		'9' : ['srv2', 'ssh -i ~/.ssh/id_rsa.bifi root@srv2.ibercivis.es'], 
-		'10' : ['srv3', 'ssh -i ~/.ssh/id_rsa.bifi root@srv3.ibercivis.es'], 
-		'11' : ['srv4', 'ssh -i ~/.ssh/id_rsa.bifi root@srv4.ibercivis.es'], 
-		'12' : ['lxbifi27', 'ssh -i ~/.ssh/id_rsa.finito finito@lxbifi27.bifi.unizar.es'], 
-		'13' : ['openstack', 'ssh -i ~/.ssh/id_rsa.bifi root@front-ec2.bifi.unizar.es'] 
+		'5' : ['acms', 'ssh -i ~/.ssh/id_rsa.finito root@acms.traci.es'],
+		'6' : ['ns3',  'ssh -i ~/.ssh/id_rsa.finito root@ns3.traci.es'], 
+		'7' : ['hispalinux', 'ssh -p 22022 -i ~/.ssh/id_rsa.finito root@pound.hispalinux.es'], 
+		'8' : ['correo',  'ssh -i ~/.ssh/id_rsa.finit oroot@correo.traci.es'], 
+		'9' : ['srv1', 'ssh -i ~/.ssh/id_rsa.bifi root@srv1.ibercivis.es'],
+		'10' : ['srv2', 'ssh -i ~/.ssh/id_rsa.bifi root@srv2.ibercivis.es'], 
+		'11' : ['srv3', 'ssh -i ~/.ssh/id_rsa.bifi root@srv3.ibercivis.es'], 
+		'12' : ['srv4', 'ssh -i ~/.ssh/id_rsa.bifi root@srv4.ibercivis.es'], 
+		'13' : ['zgz_openstack', 'ssh -i ~/.ssh/id_rsa.bifi root@front-ec2.bifi.unizar.es'],
+		'14' : ['UoW_dgserver', 'ssh -i ~/.ssh/id_rsa.finito root@161.74.91.78'], 
+		'15' : ['UoW_ngs', 'ssh -i ~/.ssh/id_rsa.finito ferrerd@ngs.wmin.ac.uk'],
+		'16' : ['UoW_test-DG-server', 'ssh -i ~/.ssh/id_rsa.finito root@10.20.151.55'],
+		'17' : ['UoW_ecs-docs', 'ssh -i ~/.ssh/id_rsa.finito root@docs.ecs.westminster.ac.uk'],
+		'18' : ['UoW_spiderman', 'ssh -i ~/.ssh/id_rsa.finito ferrerd@spiderman.cpc.wmin.ac.uk'],
+		'19' : ['UoW_dg-server2', 'ssh -i ~/.ssh/id_rsa.finito root@dg-server2.wmin.ac.uk'],
+		'20' : ['UoW_dg-portal', 'ssh -i ~/.ssh/id_rsa.finito root@dg-portal.wmin.ac.uk'],
 }
 
 ## Print all the options in the dic 
