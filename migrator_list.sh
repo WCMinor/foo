4b7870bbc2dac6e28f94a3e104b0efb768eecc1e
root <root@panel.traci.es>
2012-05-25 01:43:28 +0200

    migrator_list now look for the lists itself

--- migrator_list.sh
+++ migrator_list.sh
@@ -40,13 +40,10 @@ exit 0;
 fi
 for h in $(list)
  do 
- echo $h;
  for i in `/usr/local/bin/ezmlm/ezmlm-list /home/vpopmail/domains/$domain/$h`;
   do
-  echo "/usr/bin/mlmmj-sub -L /var/www/sites/$remote_panel_user/$domain/lists/$domain"_"$h -a $i";
-  #ssh root@$remote_host "/usr/bin/mlmmj-sub -L /var/www/sites/$remote_panel_user/$domain/lists/$domain"_"$list -a $i";
-echo "chown -R $remote_user:$remote_group /var/www/sites/$remote_panel_user/$domain/lists/$domain"_"$h";
-#ssh root@$remote_host "chown -R $remote_user:$remote_group /var/www/sites/$remote_panel_user/$domain/lists/$domain"_"$list";
+  ssh root@$remote_host "/usr/bin/mlmmj-sub -L /var/www/sites/$remote_panel_user/$domain/lists/$domain"_"$list -a $i";
+  ssh root@$remote_host "chown -R $remote_user:$remote_group /var/www/sites/$remote_panel_user/$domain/lists/$domain"_"$list";
   done
-  done
+ done
 fi
