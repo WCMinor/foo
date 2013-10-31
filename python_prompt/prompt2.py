ea84f1242f4554bd5413dac36fd5560fe8963acd
WCMinor <risingofthemoon@gmail.com>
2013-10-31 00:36:40 +0000

    new python prompt for sysadmins

--- python_prompt/prompt2.py
+++ python_prompt/prompt2.py
@@ -1,13 +1,14 @@
 import urwid
 import os
-
-
+import syslog
+server_list=os.getcwd()+'/.server.list'
+bash='/opt/local/bin/bash'
 
 def set_list(pos):
     list=[]
     servers={}
     key=1
-    with open("server.list") as sl:
+    with open(server_list) as sl:
         for line in sl:
            servers[key]=line.split()
 	   list.append(u''.join(servers[key][pos]))
@@ -17,35 +18,34 @@ def set_list(pos):
 
 choices = set_list(0)
 
-def ssh_server(option):
+def ssh_server(button,option):
+    if option=='localhost':
+        exit_program(1)
     key=1
     options = {}
-    with open("server.list") as f:
+    with open(server_list) as f:
         for line in f:
             options[int(key)] = line.split()
             key=key+1
     key=1
     for i in options:
         if options[key][0]==option:
-            key, user, url = options[key][1], options[key][2], options[key][3]
-            ssh="ssh ","-i ",key, " ", user+"@"+url
+            privkey, user, url = options[key][1], options[key][2], options[key][3]
+            ssh="ssh ","-i ",privkey, " ", user+"@"+url
+	    h="".join(ssh)
+            syslog.syslog(syslog.LOG_NOTICE, h)
+	    os.system("clear")
             os.system("".join(ssh))
+	    exit_program(1)
+        key=key+1
 
 def menu(title, choices):
     body = [urwid.Text(title), urwid.Divider()]
     for c in choices:
         button = urwid.Button(c)
-        urwid.connect_signal(button, 'click', item_chosen, c)
+        urwid.connect_signal(button, 'click', ssh_server, c)
         body.append(urwid.AttrMap(button, None, focus_map='reversed'))
     return urwid.ListBox(urwid.SimpleFocusListWalker(body))
-
-def item_chosen(button, choice):
-#    response = urwid.Text([u'You chose ', choice, u'\n'])
-    response = ssh_server(choice)
-    done = urwid.Button(u'Ok')
-    urwid.connect_signal(done, 'click', exit_program)
-    main.original_widget = urwid.Filler(urwid.Pile([response,
-        urwid.AttrMap(done, None, focus_map='reversed')]))
 
 def exit_program(button):
     raise urwid.ExitMainLoop()
@@ -56,3 +56,4 @@ top = urwid.Overlay(main, urwid.SolidFill(u'\N{MEDIUM SHADE}'),
     valign='middle', height=('relative', 60),
     min_width=20, min_height=9)
 urwid.MainLoop(top, palette=[('reversed', 'standout', '')]).run()
+os.system(bash)
