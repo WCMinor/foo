474f18daaab975faf57b2abf8a66844c90162b1d
WCMinor <risingofthemoon@gmail.com>
2014-10-21 14:39:39 +0100

    script to delete files in the guse portal changed

--- delete_guse_portal_files.py
+++ delete_guse_portal_files.py
@@ -3,7 +3,6 @@
 import fnmatch
 import os
 import subprocess
-import re
 
 #set the age of the files not to remove
 days='20'
@@ -17,36 +16,45 @@ excludes=['/srv/guse/guse/apache-tomcat-6.0.36/temp/dci_bridge',
 '/srv/guse/guse/apache-tomcat-6.0.36/temp/submittedwflog',
 '/srv/guse/guse/apache-tomcat-6.0.36/temp/users',
 '/srv/guse/guse/apache-tomcat-6.0.36/temp/WFI_logg',
-'/srv/guse/guse/apache-tomcat-6.0.36/temp/liferay']
+'/srv/guse/guse/apache-tomcat-6.0.36/temp/liferay',
+'jar']
 
 
-paths = []
+dirs = []
+files = []
 #add stuff but from the dci_bridge dir
-#add dirs
-for root, dirnames, filenames in os.walk('/srv/guse/guse/apache-tomcat-6.0.36/temp/'):
-  for path in dirnames:
-      paths.append(os.path.join(root, path))
 #add files
 for root, dirnames, filenames in os.walk('/srv/guse/guse/apache-tomcat-6.0.36/temp/'):
   for path in filenames:
-      paths.append(os.path.join(root, path))
+      files.append(os.path.join(root, path))
+#add dirs
+for root, dirnames, filenames in os.walk('/srv/guse/guse/apache-tomcat-6.0.36/temp/'):
+  for path in dirnames:
+      dirs.append(os.path.join(root, path))
 
 for exclude in excludes:
-        paths = [dir for dir in paths if not re.match(exclude, dir)]
+    dirs = [line for line in dirs if not exclude in line]
+for exclude in excludes:
+    files = [line for line in files if not exclude in line]
+
 #add stuff belonging to the submission service in the dci_bridge
-#add dirs
-for root, dirnames, filenames in os.walk('/srv/guse/guse/apache-tomcat-6.0.36/temp/dci_bridge/0/'):
-  for path in dirnames:
-      paths.append(os.path.join(root, path))
 #add files
 for root, dirnames, filenames in os.walk('/srv/guse/guse/apache-tomcat-6.0.36/temp/dci_bridge/0/'):
   for path in filenames:
-      paths.append(os.path.join(root, path))
+      files.append(os.path.join(root, path))
+
+#add dirs
+for root, dirnames, filenames in os.walk('/srv/guse/guse/apache-tomcat-6.0.36/temp/dci_bridge/0/'):
+  for path in dirnames:
+      dirs.append(os.path.join(root, path))
 
 #clean the files
 print 'cleaning files older than '+days+' days'
-for i in paths:
+for i in files:
     print 'cleaning '+i
-#uncomments the follow to do the real removal
-#    subprocess.call(['find', i, '-mtime', '+'+days, '-type', 'f', '-delete'])
-#    subprocess.call(['find', i, '-mtime', '+'+days, '-type', 'd', '-empty', '-delete'])
+    subprocess.call(['find', i, '-mtime', '+'+days, '-type', 'f', '-delete'])
+#clean the dirs
+print 'cleaning empty dirs older than '+days+' days'
+for i in dirs:
+    print 'cleaning '+i
+    subprocess.call(['find', i, '-mtime', '+'+days, '-type', 'd', '-empty', '-delete'])
